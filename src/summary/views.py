6654ad11d92e041d489ebb34b1caa373e51907e5
banyh <banyhong@gliacloud.com>
2016-12-29 10:50:25 +0800

    update summarizer

--- src/summary/views.py
+++ src/summary/views.py
@@ -7,7 +7,12 @@ from summarizer import Article, ParagraphSet, Sentence, summary_text
 
 def handle(text):
     article = Article(text)
-    first_summary = 5 if len(article) == 1 else 2
+    if len(article) == 1:
+        first_summary = 5
+    elif len(article[0]) >= 2:
+        first_summary = 2
+    else:
+        first_summary = 1
 
     para_set = article[0]
     keywords = para_set.keywords
@@ -17,10 +22,12 @@ def handle(text):
         sim.remove(0)
     else:
         sim.pop(-1)
-    sents.extend(para_set.sents[np.sort(sim)])
+    if len(sim):
+        sents.extend(para_set.sents[np.sort(sim)])
     result = []
     for sent in sents:
-        result.append((sent.seg, sent.short.seg, "_".join(keywords)))
+        # result.append((sent.seg, sent.short.seg, "_".join(keywords)))
+        result.append((sent.seg, sent.seg, "_".join(keywords)))
 
     if len(article) == 1:  # number of paragraphs is 1
         return result
@@ -28,11 +35,11 @@ def handle(text):
     for para_set in article[1:]:
         sent = para_set.summary[0]
         keywords = para_set.keywords
+        # s1, s2 = sent.seg, sent.short.seg
+        s1, s2 = sent.seg, sent.seg
         if para_set.title is not None:
-            s1 = para_set.title.seg + '<br>' + sent.seg
-            s2 = para_set.title.seg + '<br>' + sent.short.seg
-        else:
-            s1, s2 = sent.seg, sent.short.seg
+            s1 = para_set.title.seg + '<br>' + s1
+            s2 = para_set.title.seg + '<br>' + s2
         result.append((s1, s2, "_".join(keywords)))
     return result
 
