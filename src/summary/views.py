4578e12a853718ada06dbbfe037d775628b58124
banyh <banyhong@gliacloud.com>
2017-01-03 14:30:56 +0800

    add person names as keywords

--- src/summary/views.py
+++ src/summary/views.py
@@ -4,6 +4,42 @@ from datetime import datetime as dt
 import numpy as np
 from summarizer import Article, ParagraphSet, Sentence, summary_text
 
+from summarizer.lib_keyword import chunk_parse
+from nltk import RegexpChunkParser
+from nltk.chunk.regexp import ChunkRule
+from collections import Counter
+
+
+def find_person(a):
+    person = RegexpChunkParser([ChunkRule(u'<NP>+', 'noun')])
+    person2 = RegexpChunkParser([ChunkRule(u'<NP>+<NN>?<NX>', 'noun')])
+    person3 = RegexpChunkParser([ChunkRule(u'<SP>+', 'noun')])
+
+    name_set = set()
+    cnt = Counter()
+    for s in a.sents:
+        names = chunk_parse(person, s.tagged) + chunk_parse(person2, s.tagged)
+        name_set.update(names)
+        cnt.update(names)
+        names = [n.replace('_', ' ') for n in chunk_parse(person3, s.tagged) if n[0].isupper()]
+        name_set.update(names)
+        cnt.update(names)
+
+    name_set = list(name_set)
+    name_parent = list()
+    for np in name_set:
+        par = [(len(n), n) for n in name_set if n.count(np) > 0 and len(n) > len(np)]
+        if not par:
+            name_parent.append(np)
+        else:
+            name_parent.append(sorted(par)[-1][1])
+
+    for np, c in cnt.most_common():
+        i = name_set.index(np)
+        if name_parent[i] != name_set[i]:
+            cnt[name_parent[i]] += c
+    return cnt.most_common()
+
 
 def handle(text):
     article = Article(text)
@@ -15,7 +51,8 @@ def handle(text):
         first_summary = 1
 
     para_set = article[0]
-    keywords = para_set.keywords
+    keywords = para_set.keywords.tolist()
+    keywords.insert(0, find_person(article)[0][0])
     sents = [para_set[0]]  # always choose first sentence
     sim = para_set.mostsim[:first_summary].tolist()
     if sim.count(0):
@@ -34,7 +71,8 @@ def handle(text):
 
     for para_set in article[1:]:
         sent = para_set.summary[0]
-        keywords = para_set.keywords
+        keywords = para_set.keywords.tolist()
+        keywords.insert(0, find_person(article)[0][0])
         # s1, s2 = sent.seg, sent.short.seg
         s1, s2 = sent.seg, sent.seg
         if para_set.title is not None:
